c87ef462ef90a5e14beebc5e737ffb614e0bdee8
Joel Rosen <joel@velarium.com>
2022-06-15 17:35:32 -0600

    Opeartors2!

---
+++ Operators2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Operators2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle ?");
+            var radius = double.Parse(Console.ReadLine());
+
+            AreaOfCircle(radius);
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            radius = (Math.PI * radius * radius);
+            Console.WriteLine($"This is your AreaofCirle {radius}");
+            return radius;
+            
+        }
+    }
+}
